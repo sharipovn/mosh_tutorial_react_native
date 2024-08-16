1b8e37af5262296f79cca81a4519d18154133391
Nuriddin Sharipov <sharipov.n@eeho.uz>
2024-08-16 15:21:50 +0500

    icons

--- donewithit/App.js
+++ donewithit/App.js
@@ -14,22 +14,7 @@ export default function App() {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-        <Text
-        style={{
-          fontSize:30,
-          fontFamily: 'system',
-          fontStyle: 'italic',
-          fontWeight:'bold',
-          color:'tomato',
-          textTransform:'capitalize',
-          // textDecorationLine:'line-through'
-          textAlign: 'center',
-          lineHeight:25 //boshqa componenetalaga bunaqa style berib bo'lmaydi
-        }}
-        >
-          I love React Native! This is my first React Native APP
-        </Text>
-        <AppText>sssss</AppText>
+        <MaterialCommunityIcons name='email' size={60} color='red'/>
     </View>
   );
 }
